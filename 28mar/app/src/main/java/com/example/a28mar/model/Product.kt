210f55bbb2dc5e5cf729ba549033047806896c08
MikaKate <kaelakate02@gmail.com>
2023-03-29 14:27:39 +0200

    Update Product.kt
    test 2

--- 28mar/app/src/main/java/com/example/a28mar/model/Product.kt
+++ 28mar/app/src/main/java/com/example/a28mar/model/Product.kt
@@ -2,6 +2,6 @@ package com.example.a28mar.model
 
 class Product(var name: String, var price: Float) {
     fun getPriceString(): String{
-        return "Prezzon: $price€"
+        return "Prezzo: $price€"
     }
 }
